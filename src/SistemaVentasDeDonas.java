77bace495fbf91f1487a6d381ac206b124a7cafc
FrankGS777 <frankginocchio300@gmail.com>
2023-10-31 00:25:52 -0500

    Inico del proyecto

--- src/SistemaVentasDeDonas.java
+++ src/SistemaVentasDeDonas.java
@@ -13,7 +13,7 @@ public class SistemaVentasDeDonas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
