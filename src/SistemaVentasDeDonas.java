d3bafa91720c02bd26f0927d2c17b60af8f24937
Fran <Fran@Frank>
2023-11-10 15:34:41 -0500

    Borron y cuenta nueva, inicio del proyecto

--- src/SistemaVentasDeDonas.java
+++ src/SistemaVentasDeDonas.java
@@ -13,7 +13,7 @@ public class SistemaVentasDeDonas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo xd");
+        
     }
     
 }
